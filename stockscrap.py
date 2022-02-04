4808f4316b103b422bddc0a8ab369837c92d1134
kchinmay <cskck77@gmail.com>
2022-02-04 19:47:59 +0100

    code reformattig

--- stockscrap.py
+++ stockscrap.py
@@ -2,18 +2,18 @@
     sent it via email
    Author: Chinmay Kashikar
    Date: 22-01-2022"""
-
-import pandas as pd
-import requests
-from bs4 import BeautifulSoup
 import os
 import smtplib
 from datetime import datetime
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
 def mainCalculation():
     """This function calculate the stock current value by fetching
     its LTP Via yahoo"""
-    excelFile = pd.ExcelFile(r'Stock.xlsx')
-    data = pd.read_excel(excelFile, 'Position_records', skiprows=2, usecols="C:O")
+    excel_file = pd.ExcelFile(r'Stock.xlsx')
+    data = pd.read_excel(excel_file, 'Position_records', skiprows=2, usecols="C:O")
     script_name_list = data['Scrip name'].to_list()
     net_quantity_list = data['Quantity'].to_list()
     purchase_price_list = data['Purchase price'].to_list()
@@ -30,22 +30,22 @@ def mainCalculation():
     for i in range(len(script_name_list)):
         symbol = script_name_list[i]
         url = 'https://finance.yahoo.com/quote/'+symbol+'.NS'
-        r = requests.get(url, headers=headers)
-        soup = BeautifulSoup(r.text, 'html.parser')
+        request_to_make = requests.get(url, headers=headers)
+        soup = BeautifulSoup(request_to_make.text, 'html.parser')
         last_trading_stock_price = soup.find('fin-streamer', {'class':'Fw(b) Fz(36px) Mb(-4px) D(ib)'}).text
         ltp.append(last_trading_stock_price)
 
    # Pre process the LTP - String to int and remove the ,
-    process_PP = []
+    process_purchase_price = []
     for i in range(len(ltp)):
         to_find = ','
         if ltp[i].find(to_find):
             without_comma = ltp[i].replace(',', '')
-            process_PP.append(without_comma)
+            process_purchase_price.append(without_comma)
         else:
-            process_PP.append(without_comma)
+            process_purchase_price.append(without_comma)
 
-    mapping_float = map(float, process_PP)
+    mapping_float = map(float, process_purchase_price)
     new_ltp_float = list(mapping_float)
 
    # Calculating current investment value
@@ -64,33 +64,33 @@ def mainCalculation():
     list_pp = []
 
     for name, quant, purchase, sloss, ltp, target, invested, current, pross in zip(script_name_list,
-                                                                           net_quantity_list,
-                                                                           purchase_price_list,
-                                                                           trailing_stop_loss,
-                                                                           new_ltp_float,
-                                                                           target_value,
-                                                                           invested_value_list,
-                                                                           current_investment,
-                                                                           profit_loss):
+                                                                                   net_quantity_list,
+                                                                                   purchase_price_list,
+                                                                                   trailing_stop_loss,
+                                                                                   new_ltp_float,
+                                                                                   target_value,
+                                                                                   invested_value_list,
+                                                                                   current_investment,
+                                                                                   profit_loss):
         sub_pp = []
         sub_pp.append("Script Name: {name}\n Quantity Purchased: {quant}"
                       "\n Purchased Price: {purchase} \n Trailing Stop Loss: {sloss}"
                       "\n Last Trading Price : {ltp} \n Target Price : {target} "
-                      "\n Invested Value: {invested} \n Current Value: {current}" 
+                      "\n Invested Value: {invested} \n Current Value: {current}"
                       "\n Profit OR Loss: {pross}\n\n"
                       .format(name=name, quant=quant, purchase=purchase
                               , sloss=sloss, ltp=ltp, target=target, invested=invested
                               , current=current, pross=pross))
         full_status = ''.join(sub_pp)
         list_pp.append(full_status)
-    sendEmail(status='\n'.join(list_pp), sum_pross=str(pross_sum),
+    sendemail(status='\n'.join(list_pp), sum_pross=str(pross_sum),
               invested_sum=str(invested_sum), current_sum=str(curent_sum))
 
-def sendEmail(status='', sum_pross='', invested_sum='', current_sum=''):
+def sendemail(status='', sum_pross='', invested_sum='', current_sum=''):
     """This function sent email
       """
-    EMAIL_ADDRESS_ENV = os.environ.get('EMAIL_ADDRESS')
-    EMAIL_PASSWORD_ENV = os.environ.get('EMAIL_PASSWORD')
+    email_address_env = os.environ.get('EMAIL_ADDRESS')
+    email_password_env = os.environ.get('EMAIL_PASSWORD')
     msg = "\n{status} \n\n\n Summary:\n Total Positional Invested Value:{invested_sum}\n Total Positional Current Value:{current_sum}\nTotal Portfolio Profit/Loss: {sum_pross}".format(status=status, sum_pross=sum_pross, invested_sum=invested_sum, current_sum=current_sum)
     today = datetime.today().strftime('%d-%m-%Y')
     with smtplib.SMTP('smtp.gmail.com', 587)as smtp:
@@ -99,11 +99,11 @@ def sendEmail(status='', sum_pross='', invested_sum='', current_sum=''):
         smtp.ehlo()
 
 
-        smtp.login(EMAIL_ADDRESS_ENV, EMAIL_PASSWORD_ENV)
+        smtp.login(email_address_env, email_password_env)
         subject = 'Portfolio Status '
         today = datetime.today().strftime('%d-%m-%Y')
-        msg1 = f'Subject:{subject} : {today}\n\n{msg}'
-        smtp.sendmail(EMAIL_ADDRESS_ENV, EMAIL_ADDRESS_ENV, msg1)
+        message_to_display = f'Subject:{subject} : {today}\n\n{msg}'
+        smtp.sendmail(email_address_env, email_address_env, message_to_display)
         print('Email Sent Successfully')
 
 if __name__ == '__main__':
